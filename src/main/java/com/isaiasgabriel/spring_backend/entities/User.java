0fc99cd87a670a69c68dda9c3b54f0475e5ab76b
Isaías Gabriel <is.gabri3l@gmail.com>
2025-06-17 13:14:02 -0300

    feat: user entity and resource
    Created the User class representing the entity and the UserResource,
    which currently exposes a GET route at /users that returns a user.

---
+++ src/main/java/com/isaiasgabriel/spring_backend/entities/User.java
@@ -0,0 +1,79 @@
+package com.isaiasgabriel.spring_backend.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+   @Serial
+   private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+
+    public User(){};
+
+    public User(Long id, String name, String email, String password, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/isaiasgabriel/spring_backend/services/UserResource.java
@@ -0,0 +1,18 @@
+package com.isaiasgabriel.spring_backend.services;
+
+import com.isaiasgabriel.spring_backend.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/users") // request at localhost/users
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User user = new User(1L, "Maria", "maria@gmail.com","maria123" ,"4599999-9999");
+        return ResponseEntity.ok().body(user);
+    }
+}
