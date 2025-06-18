8b0389736275d921e20d691ab6954fe9269c64ed
Isaías Gabriel <is.gabri3l@gmail.com>
2025-06-17 22:23:59 -0300

    feat: Order entity

--- src/main/java/com/isaiasgabriel/spring_backend/entities/User.java
+++ src/main/java/com/isaiasgabriel/spring_backend/entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -20,6 +22,9 @@ public class User implements Serializable {
     private String email;
     private String password;
     private String phone;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){};
 
@@ -49,6 +54,10 @@ public class User implements Serializable {
 
     public String getPhone() {
         return phone;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     public void setId(Long id) {
