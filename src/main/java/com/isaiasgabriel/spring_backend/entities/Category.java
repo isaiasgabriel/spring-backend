94fe796874a0f36d028a6a877c7849cb7ea47a1b
Isaías Gabriel <is.gabri3l@gmail.com>
2025-06-18 12:11:07 -0300

    feat: many-to-many category association with JoinTable

--- src/main/java/com/isaiasgabriel/spring_backend/entities/Category.java
+++ src/main/java/com/isaiasgabriel/spring_backend/entities/Category.java
@@ -1,5 +1,6 @@
 package com.isaiasgabriel.spring_backend.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -19,7 +20,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    @Transient
+    @ManyToMany(mappedBy = "categories")
+    @JsonIgnore
     private Set<Product> products = new HashSet<Product>();
 
     public Category() {};
--- src/main/java/com/isaiasgabriel/spring_backend/entities/Product.java
+++ src/main/java/com/isaiasgabriel/spring_backend/entities/Product.java
@@ -22,8 +22,10 @@ public class Product implements Serializable {
     private Double price;
     private String imgUrl;
 
-    // We are using Set because it doesn't allow duplicate info
-    @Transient
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name="category_id"))
     private Set<Category> categories = new HashSet<Category>();
 
     public Product() {};
