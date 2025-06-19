e2e3698cbc68a0f07d4039da47c28215d17e28b3
Isaías Gabriel <is.gabri3l@gmail.com>
2025-06-19 14:50:27 -0300

    feat(UserService): update exception handler

--- src/main/java/com/isaiasgabriel/spring_backend/services/UserService.java
+++ src/main/java/com/isaiasgabriel/spring_backend/services/UserService.java
@@ -4,6 +4,7 @@ import com.isaiasgabriel.spring_backend.entities.User;
 import com.isaiasgabriel.spring_backend.repositories.UserRepository;
 import com.isaiasgabriel.spring_backend.services.exceptions.DatabaseException;
 import com.isaiasgabriel.spring_backend.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -47,9 +48,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
